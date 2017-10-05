49eeb6661483666c1e02bac0c72b6984f646e3a6
Ege <alikemal@riseup.net>
2017-10-05 13:33:45 +0300

    Added text.html.ng support

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ class HTMLLanguageClient extends AutoLanguageClient {
       gohtmlSupport,
       mustacheSupport,
     } = atom.config.get('ide-html')
-    return ['text.html.basic']
+    return ['text.html.basic', 'text.html.ng']
       .concat(gohtmlSupport ? 'text.html.gohtml' : [])
       .concat(mustacheSupport ? 'text.html.mustache' : [])
       .concat(additionalGrammars || [])
